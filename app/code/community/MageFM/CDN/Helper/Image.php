f6153fc41908e4a7cc06ffeb27a112366d84c548
Rafael Kassner <kassner@gmail.com>
2014-02-17 11:30:45 -0300

    Configuration

--- app/code/community/MageFM/CDN/Helper/Image.php
+++ app/code/community/MageFM/CDN/Helper/Image.php
@@ -5,6 +5,10 @@ class MageFM_CDN_Helper_Image extends Mage_Catalog_Helper_Image
 
     public function init(Mage_Catalog_Model_Product $product, $attributeName, $imageFile = null)
     {
+        if (!Mage::getStoreConfigFlag('magefm_cdn/general/enabled')) {
+            return parent::init($product, $attributeName, $imageFile);
+        }
+
         $this->_reset();
         $this->_setModel(Mage::getModel('catalog/product_image'));
         $this->_getModel()->setDestinationSubdir($attributeName);
@@ -16,6 +20,10 @@ class MageFM_CDN_Helper_Image extends Mage_Catalog_Helper_Image
 
     public function __toString()
     {
+        if (!Mage::getStoreConfigFlag('magefm_cdn/general/enabled')) {
+            return parent::__toString();
+        }
+
         $model = $this->_getModel();
 
         $url = array(
