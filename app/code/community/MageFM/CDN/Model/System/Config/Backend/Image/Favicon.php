c75d8f6ea8a2aff3f3d3ad7a9d7c7665722952d3
Rafael Kassner <kassner@gmail.com>
2014-03-14 15:39:33 -0300

    Fix #4: Favicon upload broken

---
+++ app/code/community/MageFM/CDN/Model/System/Config/Backend/Image/Favicon.php
@@ -0,0 +1,52 @@
+<?php
+
+class MageFM_CDN_Model_System_Config_Backend_Image_Favicon extends Mage_Adminhtml_Model_System_Config_Backend_Image_Favicon
+{
+
+    protected function _getUploadDir()
+    {
+        return $this->_appendScopeInfo(self::UPLOAD_DIR);
+    }
+
+    protected function _beforeSave()
+    {
+        $value = $this->getValue();
+        if ($_FILES['groups']['tmp_name'][$this->getGroupId()]['fields'][$this->getField()]['value']) {
+
+            $uploadDir = $this->_getUploadDir();
+
+            try {
+                $file = array();
+                $tmpName = $_FILES['groups']['tmp_name'];
+                $file['tmp_name'] = $tmpName[$this->getGroupId()]['fields'][$this->getField()]['value'];
+                $name = $_FILES['groups']['name'];
+                $file['name'] = $name[$this->getGroupId()]['fields'][$this->getField()]['value'];
+                $uploader = new MageFM_CDN_Model_Uploader($file);
+                $uploader->setAllowedExtensions($this->_getAllowedExtensions());
+                $uploader->setAllowRenameFiles(true);
+                $uploader->addValidateCallback('size', $this, 'validateMaxSize');
+                $result = $uploader->save($uploadDir);
+            } catch (Exception $e) {
+                Mage::throwException($e->getMessage());
+                return $this;
+            }
+
+            $filename = $result['file'];
+            if ($filename) {
+                if ($this->_addWhetherScopeInfo()) {
+                    $filename = $this->_prependScopeInfo($filename);
+                }
+                $this->setValue($filename);
+            }
+        } else {
+            if (is_array($value) && !empty($value['delete'])) {
+                $this->setValue('');
+            } else {
+                $this->unsValue();
+            }
+        }
+
+        return $this;
+    }
+
+}
