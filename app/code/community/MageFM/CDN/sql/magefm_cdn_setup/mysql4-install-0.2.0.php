b698edfab083dc19724a1f61bfb9154b75a855cf
Rafael Kassner <kassner@gmail.com>
2014-08-15 15:00:05 -0300

    Index on magefm_cdn_cache table (path limited to 512)

--- app/code/community/MageFM/CDN/sql/magefm_cdn_setup/mysql4-install-0.2.0.php
+++ app/code/community/MageFM/CDN/sql/magefm_cdn_setup/mysql4-install-0.2.0.php
@@ -3,12 +3,17 @@
 $this->startSetup();
 
 $this->run("
+
 CREATE TABLE `{$this->getTable('magefm_cdn/cache')}` (
     id integer UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     store_id smallint UNSIGNED NOT NULL,
     path text NOT NULL,
     created_at timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB ;
+
+CREATE TABLE `magefm_cdn_cache_idx1`
+    ON `{$this->getTable('magefm_cdn/cache')}` (store_id, path(512));
+
 ");
 
 $this->endSetup();
